742e5d64c636a2191960eae3620d7bb0d642ce6d
EdvaldoJose <edvaldojose003@gmail.com>
2023-12-13 17:02:00 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,37 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+/**
+ * The type Pessoa.
+ */
+public abstract class Pessoa {
+  private String nome;
+
+  /**
+   * Instantiates a new Pessoa.
+   */
+  public Pessoa() {
+
+  }
+
+  public Pessoa(String nome) {
+    this.nome = nome;
+  }
+
+  /**
+   * Gets nome.
+   *
+   * @return the nome
+   */
+  public String getNome() {
+    return nome;
+  }
+
+  /**
+   * Sets nome.
+   *
+   * @param nome the nome
+   */
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
