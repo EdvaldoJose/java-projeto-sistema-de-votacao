15fde54322e79bc88e4c2bbb5d47d1e19068687f
EdvaldoJose <edvaldojose003@gmail.com>
2023-12-13 22:53:18 -0300

    feat: Requisito 6. :rocket:

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -6,7 +6,7 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   private final ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
   private final ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
-  private ArrayList<String> cpfsComputados = new ArrayList<>();
+  private final ArrayList<String> cpfsComputados = new ArrayList<>();
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
@@ -21,8 +21,8 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-    for(PessoaEleitora pe : pessoasEleitoras){
-      if(pe.getCpf() == cpf){
+    for (PessoaEleitora pe : pessoasEleitoras) {
+      if (pe.getCpf() == cpf){
         System.out.println("Pessoa eleitora já cadastrada!");
       }
     }
@@ -32,11 +32,38 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    for (String cpf : cpfsComputados) {
+      if (cpf.equals(cpfPessoaEleitora)) {
+        System.out.println("Pessoa eleitora já votou!");
+        return;
+      }
+    }
 
+    for (PessoaCandidata pc : pessoasCandidatas) {
+      if (pc.getNumero() == numeroPessoaCandidata) {
+        pc.receberVoto();
+        cpfsComputados.add(cpfPessoaEleitora);
+        return;
+      }
+    }
+
+    System.out.println("Pessoa eleitora ou pessoa candidata não encontrada!");
   }
 
   @Override
   public void mostrarResultado() {
+    if (cpfsComputados.isEmpty()) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.");
+    } else {
+      for (PessoaCandidata pc : pessoasCandidatas){
+        System.out.println(
+            "Nome: " + pc.getNome() +
+                " - " + pc.getVotos() + " votos " +
+                "( " + (pc.getVotos() * 100 / cpfsComputados.size()) + "%)"
+        );
+      }
 
+      System.out.println("Total de votos: " + cpfsComputados.size());
+    }
   }
 }
